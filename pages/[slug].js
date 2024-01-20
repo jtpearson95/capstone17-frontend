298347dcdba284d48764e33b88ac99051a4c91bc
jtpearson95 <130475233+jtpearson95@users.noreply.github.com>
2024-01-20 10:54:04 -0500

    slug test

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,88 +1,86 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import React from "react";
-import { GET_ALL_SLUGS, GET_RESTAURANT_DISHES } from "../graphql/queries";
-import {
-  Card,
-  CardBody,
-  CardTitle,
-  CardText,
-  Col,
-  Row,
-  CardImg,
-  Button
-} from "reactstrap";
+// import { ApolloClient, InMemoryCache } from "@apollo/client";
+// import React from "react";
+// import { GET_ALL_SLUGS, GET_RESTAURANT_DISHES } from "../graphql/queries";
+// import {
+//   Card,
+//   CardBody,
+//   CardTitle,
+//   CardText,
+//   Col,
+//   Row,
+//   CardImg,
+//   Button
+// } from "reactstrap";
 
-const STRAPI_URL = process.env.STRAPI_URL || "https://capstone17-3fc1d2cfc034.herokuapp.com";
+// const STRAPI_URL = process.env.STRAPI_URL || "https://capstone17-3fc1d2cfc034.herokuapp.com";
 
 // const client = new ApolloClient({
 //   uri: `${STRAPI_URL}`,
 //   cache: new InMemoryCache(),
 // });
 
-export default function RestaurantDishes({ restaurant }) {
-  return (
-    <div>
-      <h1>{restaurant.name}</h1>
-      <Row>
-        {restaurant.dishes.map((dish, index) => (
-          <Col key={index} xs="6" sm="4">
-            <Card>
-              <CardBody>
-                <CardTitle>{dish.name}</CardTitle>
-                <CardText>${dish.price}</CardText>
-                <CardText>{dish.description}</CardText>
-                <CardImg
-                  top={true}
-                  style={{ height: 200 }}
-                  src={`${STRAPI_URL}${dish.image}`}
-                />
-                <Button>Add to Cart</Button>
-              </CardBody>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </div>
-  );
-}
+// export default function RestaurantDishes({ restaurant }) {
+//   return (
+//     <div>
+//       <h1>{restaurant.name}</h1>
+//       <Row>
+//         {restaurant.dishes.map((dish, index) => (
+//           <Col key={index} xs="6" sm="4">
+//             <Card>
+//               <CardBody>
+//                 <CardTitle>{dish.name}</CardTitle>
+//                 <CardText>${dish.price}</CardText>
+//                 <CardText>{dish.description}</CardText>
+//                 <CardImg
+//                   top={true}
+//                   style={{ height: 200 }}
+//                   src={`${STRAPI_URL}${dish.image}`}
+//                 />
+//                 <Button>Add to Cart</Button>
+//               </CardBody>
+//             </Card>
+//           </Col>
+//         ))}
+//       </Row>
+//     </div>
+//   );
+// }
 
-export async function getStaticPaths() {
-  // const { data } = await client.query({ query: GET_ALL_SLUGS });
-  const { data } = null;
+// export async function getStaticPaths() {
+//   const { data } = await client.query({ query: GET_ALL_SLUGS });
 
-  const paths = data.restaurants.data.map((restaurant) => {
-    return { params: { slug: restaurant.attributes.urlSlug } };
-  });
+//   const paths = data.restaurants.data.map((restaurant) => {
+//     return { params: { slug: restaurant.attributes.urlSlug } };
+//   });
 
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
-export async function getStaticProps({ params }) {
-  // const { data } = await client.query({
-  //   query: GET_RESTAURANT_DISHES,
-  //   variables: { slugUrl: params.slug },
-  // });
-  const {data} = null;
+// export async function getStaticProps({ params }) {
+//   const { data } = await client.query({
+//     query: GET_RESTAURANT_DISHES,
+//     variables: { slugUrl: params.slug },
+//   });
 
-  const attrs = data.restaurants.data[0].attributes;
+//   const attrs = data.restaurants.data[0].attributes;
 
-  const dishes = attrs.dishes.data.map((dish) => ({
-    name: dish.attributes.name,
-    price: dish.attributes.price,
-    description: dish.attributes.description,
-    image: dish.attributes.image.data.attributes.url,
-    // Add other dish properties as needed
-  }));
+//   const dishes = attrs.dishes.data.map((dish) => ({
+//     name: dish.attributes.name,
+//     price: dish.attributes.price,
+//     description: dish.attributes.description,
+//     image: dish.attributes.image.data.attributes.url,
+//     // Add other dish properties as needed
+//   }));
 
-  return {
-    props: {
-      restaurant: {
-        name: attrs.name,
-        dishes,
-      },
-    },
-  };
-}
+//   return {
+//     props: {
+//       restaurant: {
+//         name: attrs.name,
+//         dishes,
+//       },
+//     },
+//   };
+// }
